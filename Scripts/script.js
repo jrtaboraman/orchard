01d8deaf273967b1b7254521274f1ea3a6e6fa97
francisco.tabora <francisco.tabora@volenday.com>
2022-11-28 21:23:13 +0800

    pop up modal added

---
+++ Scripts/script.js
@@ -0,0 +1,20 @@
+function toggle1() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup1 = document.getElementById("popup1");
+  popup1.classList.toggle("active");
+}
+
+function toggle2() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup2 = document.getElementById("popup2");
+  popup2.classList.toggle("active");
+}
+
+function toggle3() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup3 = document.getElementById("popup3");
+  popup3.classList.toggle("active");
+}
